6fc2c0172e2e7f8a52b2f498065bbdd822381cbe
antoinecollas <contact@antoinecollas.fr>
2019-12-12 20:38:01 +0100

    fix

--- examples/UAVSAR/compute_ROC_adaptative_rank.py
+++ examples/UAVSAR/compute_ROC_adaptative_rank.py
@@ -40,133 +40,7 @@ from LRST.low_rank_statistics import *
 from LRST.proportionality_statistics import *
 from LRST.read_sar_data import *
 from LRST.wavelet_functions import *
-
-def download_uavsar_cd_dataset(path='./data/'):
-
-    # if directory exists just catch error
-    try:
-        os.mkdir(path)
-    except:
-        path
-
-    # Links to UAVSAR datasets
-    list_of_files = [
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_09014_007_090423_L090HH_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_09014_007_090423_L090HV_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_09014_007_090423_L090VV_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_09014_007_090423_L090HH_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_09014_007_090423_L090HV_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_09014_007_090423_L090VV_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_12021_007_120427_L090HH_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_12021_007_120427_L090HV_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_12021_007_120427_L090VV_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_12021_007_120427_L090HH_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_12021_007_120427_L090HV_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_12021_007_120427_L090VV_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_14006_007_140117_L090HH_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_14006_007_140117_L090HV_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_14006_007_140117_L090VV_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_14006_007_140117_L090HH_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_14006_007_140117_L090HV_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_14006_007_140117_L090VV_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_15059_006_150511_L090HH_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_15059_006_150511_L090HV_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_15059_006_150511_L090VV_03_BC_s4_1x1.slc',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_15059_006_150511_L090HH_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_15059_006_150511_L090HV_03_BC.ann',
-                    'http://downloaduav2.jpl.nasa.gov/Release25/SanAnd_26524_03/SanAnd_26524_15059_006_150511_L090VV_03_BC.ann']
-
-    for file_url in list_of_files:
-        if not os.path.exists(path + file_url.split('/')[-1]):
-            if not os.path.exists(path):
-                os.makedirs(path)
-            import wget
-            print("File %s not found, downloading it" % file_url.split('/')[-1])
-            wget.download(url=file_url, out=path+file_url.split('/')[-1])
-
-def load_UAVSAR(path, debug, full_time_series, scene_number=1):
-    """ Function which loads UAVSAR time series with its ground truth. It also applies a wavelet decomposition.
-
-    Inputs:
-        * path = base path to find the data
-        * debug = if true, only a a small square of the time series is loaded
-        * full_time_series = if true, loads the full time series, otherwhise loads only first and last images
-        * scene_number = 1 or 2, 2 different scenes are available
-    Outputs:
-        * image = the time series of shape (rows, columns, p, T)
-        * ground truth the time series (rows, columns)"""
-
-    if scene_number==1:
-        crop_indexes = [28891,31251,2891,3491]
-    elif scene_number==2:
-        crop_indexes = [25601,27901,3236,3836]
-    else:
-        print("ERROR in number of scene....")
-        import sys
-        sys.exit(1)
-
-    # Downloading data if needed
-    download_uavsar_cd_dataset(path=path)
-    
-    # Reading data using the class
-    print( '|￣￣￣￣￣￣￣￣|')
-    print( '|   READING     |') 
-    print( '|   dataset     |')
-    print( '|               |' )  
-    print( '| ＿＿＿_＿＿＿＿|') 
-    print( ' (\__/) ||') 
-    print( ' (•ㅅ•) || ')
-    print( ' / 　 づ')
-    data_class = uavsar_slc_stack_1x1(path)
-    data_class.read_data(time_series=full_time_series, polarisation=['HH', 'HV', 'VV'], segment=4, crop_indexes=crop_indexes)
-    if debug:
-        n_r, n_rc, _, _ = data_class.data.shape
-        new_size_image = 200
-        data_class.data = data_class.data[(n_r//2)-(new_size_image//2):(n_r//2)+(new_size_image//2), (n_rc//2)-(new_size_image//2):(n_rc//2)+(new_size_image//2), :, :]
-    print('Done')
-
-    # Spatial vectors
-    center_frequency = 1.26e+9 # GHz, for L Band
-    bandwith = float(data_class.meta_data['SanAnd_26524_09014_007_090423_L090HH_03_BC']['Bandwidth']) * 10**6 # Hz
-    range_resolution = float(data_class.meta_data['SanAnd_26524_09014_007_090423_L090HH_03_BC']['1x1 SLC Range Pixel Spacing']) # m, for 1x1 slc data
-    azimuth_resolution = float(data_class.meta_data['SanAnd_26524_09014_007_090423_L090HH_03_BC']['1x1 SLC Azimuth Pixel Spacing']) # m, for 1x1 slc data
-
-    number_pixels_azimuth, number_pixels_range, p, T = data_class.data.shape
-
-    range_vec = np.linspace(-0.5,0.5,number_pixels_range) * range_resolution * number_pixels_range
-    azimuth_vec = np.linspace(-0.5,0.5,number_pixels_azimuth) * azimuth_resolution * number_pixels_azimuth
-    Y, X = np.meshgrid(range_vec,azimuth_vec)
-
-    # Decomposition parameters (j=1 always because reasons)
-    R = 2
-    L = 2
-    d_1 = 10
-    d_2 = 10
-
-    # Wavelet decomposition of the time series
-    print( '|￣￣￣￣￣￣￣￣|')
-    print( '|   Wavelet     |') 
-    print( '| decomposition |')
-    print( '|               |' )  
-    print( '| ＿＿＿_＿＿＿＿|') 
-    print( ' (\__/) ||') 
-    print( ' (•ㅅ•) || ')
-    print( ' / 　 づ')
-    image = np.zeros((number_pixels_azimuth, number_pixels_range, p*R*L, T), dtype=complex)
-    for t in range(T):
-        for i_p in range(p):
-            image_temp = decompose_image_wavelet(data_class.data[:,:,i_p,t], bandwith, range_resolution, azimuth_resolution, center_frequency,
-                                    R, L, d_1, d_2)
-            image[:,:,i_p*R*L:(i_p+1)*R*L, t] = image_temp
-    image_temp = None
-    print('Done')
-
-    path_ground_truth = './data/ground_truth_uavsar_scene'+str(scene_number)+'.npy'
-    ground_truth_original = np.load(path_ground_truth)
-    if debug:
-        ground_truth_original = ground_truth_original[(n_r//2)-(new_size_image//2):(n_r//2)+(new_size_image//2), (n_rc//2)-(new_size_image//2):(n_rc//2)+(new_size_image//2)]
-
-    return image, ground_truth_original, X, Y
+from compute_ROC_UAVSAR_dataset import load_UAVSAR
 
 if __name__ == '__main__':
     # Activate latex in figures (or not)
@@ -187,7 +61,7 @@ if __name__ == '__main__':
     # data
 
     # DEBUG mode for fast debugging (use a small patch)
-    DEBUG = False
+    DEBUG = True
     PATH = 'data/UAVSAR/'
     FULL_TIME_SERIES = False # if true: use the full time series, else: use only the first and last images of the time series
     SCENE_NUMBER = 1
@@ -221,9 +95,9 @@ if __name__ == '__main__':
     print("Elpased time: %d s" %(time.time()-t_beginning))
 
     # Test LRCG with EDC
-    statistic_list = [scale_and_shape_equality_robust_statistic, scale_and_shape_equality_robust_statistic_low_rank]
-    statistic_names = ['$\hat{\Lambda}_{\mathrm{CG}}$', '$\hat{\Lambda}_{\mathrm{LRCG, R=EDC}}$']
-    args_list = [(0.01, 20, 'log'), (0.01, 20, 'EDC', False, 'log')]
+    statistic_list = [covariance_equality_glrt_gaussian_statistic, LR_CM_equality_test]
+    statistic_names = ['$\hat{\Lambda}_{\mathrm{G}}$', '$\hat{\Lambda}_{\mathrm{LRG, R=EDC}}$']
+    args_list = [('log'), ('EDC', False, 'log')]
 
     if not (len(statistic_list)==len(statistic_names)==len(args_list)):
         print('ERROR')
@@ -288,6 +162,7 @@ if __name__ == '__main__':
             pd_array_ranks[rank, i_λ] = good_detection.sum() / (ground_truth_rank==1).sum()
             pfa_array_ranks[rank, i_λ] = false_alarms.sum() / (ground_truth_rank==0).sum()
 
+    number_of_points = number_of_points//10
     pfa_target = np.linspace(1/number_of_points, 1, num=number_of_points)
     for i, pfa in enumerate(pfa_target):
         pd = 0
@@ -298,8 +173,8 @@ if __name__ == '__main__':
             relative_error = np.abs(pfa-pfa_array_ranks[rank,j])/pfa
             if relative_error>=0.05:
                 print('High relative error on Pfa:', relative_error)
-            nb_points = (ranks == rank).sum()
-            pd += nb_points*pd_array_ranks[rank, j]
+            nb_points_rank = (ranks == rank).sum()
+            pd += nb_points_rank*pd_array_ranks[rank, j]
         pfa_array[i, 1] = pfa
         pd_array[i, 1] = pd/(results.shape[0]*results.shape[1])
 
